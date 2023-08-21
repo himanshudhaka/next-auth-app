1bab89c3cdc5b7a48f77bf0d493e40e80c6836e3
Himanshu Dhaka <himanshudhaka1701@gmail.com>
2023-08-21 22:04:11 +0530

    changes done

--- app/page.js
+++ app/page.js
@@ -1,3 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>hi</div>;
+  return (
+    <div className="h-full flex justify-center items-center">
+      <Link
+        href="/login"
+        className="flex justify-center rounded-md bg-black px-6 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+      >
+        Login
+      </Link>
+    </div>
+  );
 }
